9bf5be76b0eef8cb797046647d3320b1ad822252
D3a7hKn1gh7 <angelpisarev@gmail.com>
2026-01-17 17:37:10 +0200

    Initial project setup with file header

---
+++ Uno/Uno.cpp
@@ -0,0 +1,15 @@
+/*
+* Angel Pisarev group 2
+* 3MI0600689
+* GCC
+* Main file for UNO card game implementation
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "UNO Game - Project Start" << endl;
+    return 0;
+}
