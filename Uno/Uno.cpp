5acdd39bf11aaca7e89c60f877f800221373a0eb
D3a7hKn1gh7 <angelpisarev@gmail.com>
2026-01-18 12:33:36 +0200

    Implement special card effects
    and also a function which gets to the next player index

--- Uno/Uno.cpp
+++ Uno/Uno.cpp
@@ -5,6 +5,7 @@
 * Main file for UNO card game implementation
 */
 
+
 #include <iostream>
 #include <algorithm>
 #include <random>
@@ -225,25 +226,20 @@ bool isValidPlay(const Card& card, const Card& topCard) {
 
 // Draw one card from deck for a player
 void drawCard(GameState& game, int playerIndex) {
-    // Check if deck is empty - reshuffle discard pile
     if (game.deckSize == 0) {
         cout << "Deck empty! Reshuffling discard pile...\n";
 
-        // Move all cards except top card from discard to deck
         for (int i = 0; i < game.discardSize - 1; i++) {
             game.deck[game.deckSize] = game.discardPile[i];
             game.deckSize = game.deckSize + 1;
         }
 
-        // Keep only top card in discard pile
         game.discardPile[0] = game.discardPile[game.discardSize - 1];
         game.discardSize = 1;
 
-        // Shuffle the deck
         shuffleDeck(game);
     }
 
-    // Draw card if deck has cards and player has space
     if (game.deckSize > 0 && game.players[playerIndex].cardCount < MAX_HAND_SIZE) {
         game.deckSize = game.deckSize - 1;
         game.players[playerIndex].hand[game.players[playerIndex].cardCount] = game.deck[game.deckSize];
@@ -255,31 +251,101 @@ void drawCard(GameState& game, int playerIndex) {
     }
 }
 
+// Get the index of the next player based on direction
+int getNextPlayer(const GameState& game) {
+    if (game.clockwise) {
+        return (game.currentPlayer + 1) % game.numPlayers;
+    }
+    else {
+        return (game.currentPlayer - 1 + game.numPlayers) % game.numPlayers;
+    }
+}
+
+// Apply special card effects (Skip, Reverse, +2, Wild+4)
+void applyCardEffect(GameState& game, const Card& card) {
+    if (card.value == VALUE_SKIP) {
+        cout << "Skip! Player " << (getNextPlayer(game) + 1) << " is skipped!\n";
+        game.currentPlayer = getNextPlayer(game);
+    }
+    else if (card.value == VALUE_REVERSE) {
+        game.clockwise = !game.clockwise;
+        cout << "Reverse! Direction changed!\n";
+
+        // In 2-player game, Reverse acts like Skip
+        if (game.numPlayers == 2) {
+            game.currentPlayer = getNextPlayer(game);
+        }
+    }
+    else if (card.value == VALUE_DRAW_TWO) {
+        int nextPlayer = getNextPlayer(game);
+        cout << "Player " << (nextPlayer + 1) << " draws 2 cards!\n";
+
+        drawCard(game, nextPlayer);
+        drawCard(game, nextPlayer);
+
+        // Fixed: Player does NOT skip turn after drawing
+    }
+    else if (card.value == VALUE_WILD_DRAW_FOUR) {
+        int nextPlayer = getNextPlayer(game);
+        cout << "Player " << (nextPlayer + 1) << " draws 4 cards!\n";
+
+        for (int i = 0; i < 4; i++) {
+            drawCard(game, nextPlayer);
+        }
+
+        // Fixed: Player does NOT skip turn after drawing
+    }
+}
+
 int main() {
     srand(time(0));
 
     GameState game;
     game.deckSize = 0;
     game.discardSize = 0;
-    game.numPlayers = 2;
+    game.numPlayers = 3;
+    game.currentPlayer = 0;
+    game.clockwise = true;
 
     initializeDeck(game);
     shuffleDeck(game);
     dealCards(game);
 
-    cout << "=== Draw Card Test ===\n\n";
-    cout << "Player 1 initial hand:\n";
-    printPlayerHand(game, 0);
+    cout << "=== Special Card Effects Test ===\n\n";
 
-    cout << "\nDrawing 3 cards...\n";
-    drawCard(game, 0);
-    drawCard(game, 0);
-    drawCard(game, 0);
+    // Test Skip card
+    Card skipCard;
+    skipCard.color = COLOR_RED;
+    skipCard.value = VALUE_SKIP;
 
-    cout << "\nPlayer 1 hand after drawing:\n";
-    printPlayerHand(game, 0);
+    cout << "Current player: " << (game.currentPlayer + 1) << "\n";
+    cout << "Playing: ";
+    printCard(skipCard);
+    cout << "\n";
+    applyCardEffect(game, skipCard);
+    cout << "After effect, current player: " << (game.currentPlayer + 1) << "\n\n";
 
-    cout << "\nCards remaining in deck: " << game.deckSize << "\n";
+    // Test Reverse card
+    Card reverseCard;
+    reverseCard.color = COLOR_BLUE;
+    reverseCard.value = VALUE_REVERSE;
+
+    cout << "Playing: ";
+    printCard(reverseCard);
+    cout << "\n";
+    applyCardEffect(game, reverseCard);
+    cout << "\n";
+
+    // Test Draw Two card
+    Card drawTwoCard;
+    drawTwoCard.color = COLOR_GREEN;
+    drawTwoCard.value = VALUE_DRAW_TWO;
+
+    cout << "Playing: ";
+    printCard(drawTwoCard);
+    cout << "\n";
+    applyCardEffect(game, drawTwoCard);
+    cout << "\n";
 
     return 0;
 }
